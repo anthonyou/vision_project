e9d732c3177320305b38cea9532d6055cccf72e3
Manel Baradad <manelbaradad@gmail.com>
2023-01-20 16:23:59 -0500

    Added visdom env

--- main_simulation.py
+++ main_simulation.py
@@ -1,7 +1,9 @@
 import os
 # Manel: Hardcoded visdom environment. I have this on my path, but we can use the same.
 # you need to launch the following command, if it's not already running:
+# results are displayed in http://visiongpu09.csail.mit.edu:12890/
 os.environ['VISDOM_HOST'] = 'visiongpu09:12890'
+visdom_environment = 'inverse_vision_' + os.environ['USER']
 
 from my_python_utils.common_utils import *
 from inverse_problems import *
@@ -11,7 +13,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Main method')
   parser.add_argument("--img-file", type=str, default='img_examples/pug.png', help="img path to test")
   parser.add_argument('--problem-type', type=str, default='random', choices=inverse_problems.keys())
-  parser.add_argument('--solver-method', type=str, default='explicit', choices=['explicit', 'img2img', 'masking'])
+  parser.add_argument('--solver-method', type=str, default='explicit', choices=solvers.keys())
   parser.add_argument('--seed', default=1337, type=int)
 
   # params for img2img solver, ignored if other method is used
@@ -26,7 +28,7 @@ if __name__ == '__main__':
 
   reconstruction, logs = solver.solve()
 
-  imshow(img, title='gt')
-  imshow(img, title='observation')
-  imshow(reconstruction, title='reconstruction')
+  imshow(img, title='gt', env=visdom_environment)
+  imshow(img, title='observation', env=visdom_environment)
+  imshow(reconstruction, title='reconstruction', env=visdom_environment)
 
